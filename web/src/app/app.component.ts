2304235a8faf850ae6dc014f29d14c0ababc1d8b
Mailson Teles Borges <mailson.borges@sou.unijui.edu.br>
2025-02-07 18:26:10 -0300

    Add global styles, integrate Roboto font, and configure translation service

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '@app/components/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, SidebarComponent],
 })
-export class AppComponent {
-  title = 'web';
-}
+export class AppComponent {}

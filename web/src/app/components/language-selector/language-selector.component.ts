10cb0c93515e4f361585a3df18e77e6fdd39513c
Mailson Teles Borges <mailson.borges@sou.unijui.edu.br>
2025-02-09 16:17:10 -0300

    Add Language Selector component with ng-select integration

---
+++ web/src/app/components/language-selector/language-selector.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgOptionComponent, NgSelectComponent } from '@ng-select/ng-select';
+
+@Component({
+  selector: 'app-language-selector',
+  templateUrl: './language-selector.component.html',
+  styleUrl: './language-selector.component.scss',
+  imports: [FormsModule, NgSelectComponent, NgOptionComponent],
+})
+export class LanguageSelectorComponent {
+  @Input()
+  selectedLanguage: string = 'pt';
+
+  languages = [
+    { id: 'pt', name: 'Português', flag: 'https://flagcdn.com/br.svg' },
+    { id: 'es', name: 'Español', flag: 'https://flagcdn.com/es.svg' },
+    { id: 'en', name: 'English', flag: 'https://flagcdn.com/gb.svg' },
+  ];
+}

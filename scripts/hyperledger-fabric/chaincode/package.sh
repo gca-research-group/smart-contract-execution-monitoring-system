57a56bdf44cf695288910efcb36dbaa7ec007602
Mailson Teles Borges <mailson.borges@sou.unijui.edu.br>
2025-02-12 14:55:16 -0300

    Add scripts for chaincode installation and packaging

--- scripts/hyperledger-fabric/chaincode/package.sh
+++ scripts/hyperledger-fabric/chaincode/package.sh
@@ -3,19 +3,21 @@ source ./scripts/config/_icons.sh
 
 CONTAINER_NAME=peer0.manufacturer.supplychain.com
 
-BASE_PATH=/etc/hyperledger/fabric
 CC_LABEL=supplychain.1.0-1.0
+
+BASE_PATH=/etc/hyperledger/fabric
 CHAINCODE_PATH=$BASE_PATH/chaincode
-CC_PACKAGE_FILE=$CHAINCODE_PATH/$CC_LABEL.tar.gz
-SMART_CONTRACT=$CHAINCODE_PATH/supplychain.go
+CC_PACKAGE_FILE=$CC_LABEL.tar.gz
+SMART_CONTRACT=supplychain.go
 
 echo -e "${PROCESSING_ICON} Installing dependencies."
-COMMAND="cd $CHAINCODE_PATH && go mod tidy && go mod vendor"
+COMMAND="cd $CHAINCODE_PATH && go mod tidy"
 docker exec -it $CONTAINER_NAME bash -c "$COMMAND"
 echo -e "${SUCCESS_ICON} Dependencies installed."
 
 echo -e "${PROCESSING_ICON} Packaging."
 COMMAND="cd $CHAINCODE_PATH && peer lifecycle chaincode package $CC_PACKAGE_FILE -p $SMART_CONTRACT --label $CC_LABEL"
+
 docker exec -it $CONTAINER_NAME bash -c "$COMMAND"
 echo -e "${SUCCESS_ICON} Packaged."
 

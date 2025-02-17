cb515f84551fc994830ed198db2d57f16daa4964
Mailson Teles Borges <mailson.borges@sou.unijui.edu.br>
2025-02-17 15:18:50 -0300

    Refactor LanguageSelector component to use Reactive Forms and update language selection handling

--- web/src/app/components/language-selector/language-selector.component.ts
+++ web/src/app/components/language-selector/language-selector.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, effect, model } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CustomControlValueAccessorDirective } from '@app/directives/custom-control-value-accessor';
 import { NgOptionComponent, NgSelectComponent } from '@ng-select/ng-select';
 
@@ -7,18 +7,30 @@ import { NgOptionComponent, NgSelectComponent } from '@ng-select/ng-select';
   selector: 'app-language-selector',
   templateUrl: './language-selector.component.html',
   styleUrl: './language-selector.component.scss',
-  imports: [FormsModule, NgSelectComponent, NgOptionComponent],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgSelectComponent,
+    NgOptionComponent,
+  ],
 })
-export class LanguageSelectorComponent extends CustomControlValueAccessorDirective {
-  @Input()
-  current = 'pt';
-
-  @Output()
-  select = new EventEmitter<string>();
+export class LanguageSelectorComponent extends CustomControlValueAccessorDirective<string> {
+  language = model('pt');
 
   languages = [
     { id: 'pt', name: 'Português', flag: 'https://flagcdn.com/br.svg' },
     { id: 'es', name: 'Español', flag: 'https://flagcdn.com/es.svg' },
     { id: 'en', name: 'English', flag: 'https://flagcdn.com/gb.svg' },
   ];
+
+  constructor() {
+    super();
+    effect(() => {
+      this.formControl.patchValue(this.language());
+    });
+  }
+
+  change(language: string) {
+    this.language.update(() => language);
+  }
 }

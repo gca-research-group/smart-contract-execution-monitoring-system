70cadb9ab5f9508b238a26b6ca3602cd862626bc
Mailson Teles Borges <mailson.borges@sou.unijui.edu.br>
2025-03-05 11:57:45 -0300

    Remove Hyperledger Fabric Scripts

--- scripts/hyperledger-fabric/chaincode/queryinstalled.sh
+++
@@ -1,4 +0,0 @@
-CORE_PEER_MSPCONFIGPATH="$BASE_PATH/crypto-config/users/Admin@org1.example.com/msp"
-
-export CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/fabric/crypto-config/users/Admin@org1.example.com/msp
-peer lifecycle chaincode queryinstalled
--- scripts/hyperledger-fabric/network/launch.sh
+++
@@ -1,8 +0,0 @@
-echo -e "========= Clean ========="
-./scripts/hyperledger-fabric/network/down.sh
-
-echo -e "\n========= Up ========="
-./scripts/hyperledger-fabric/network/up.sh
-
-echo -e "\n========= Channel ========="
-./scripts/hyperledger-fabric/network/channel.sh

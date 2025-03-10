8e5b7f30b8e33e36fb6bf8347e08f1a5ccb57281
Mailson Teles Borges <mailson.borges@sou.unijui.edu.br>
2025-03-10 17:21:43 -0300

    Enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  });
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`Server started on port: ${port}`);

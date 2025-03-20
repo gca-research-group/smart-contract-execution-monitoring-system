7457ce9a40dc26ae0b1bb3f7be6c576786990a44
Mailson Teles Borges <mailson.borges@sou.unijui.edu.br>
2025-03-20 16:05:55 -0300

    Remove smart contract seed

--- api/src/database/seeds/index.ts
+++ api/src/database/seeds/index.ts
@@ -1,10 +1,5 @@
 import * as user from './1741269990949-users';
-import * as smartcontracts from './1742269990949-smartcontracts';
 
 user.seed().catch((error) => {
-  console.error('Seeding failed:', error);
-});
-
-smartcontracts.seed().catch((error) => {
   console.error('Seeding failed:', error);
 });

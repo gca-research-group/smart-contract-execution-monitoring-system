d29add6e68f41bb60fd851eb0e9b28274794f871
Mailson Teles Borges <mailson.borges@sou.unijui.edu.br>
2025-05-22 08:42:46 -0300

    Extract service configurations to modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { PUBLIC_FOLDER } from './const';
-import { typeorm } from './database/typeorm.config';
 import { AuthModule } from './modules/auth';
 import { BlockchainModule } from './modules/blockchain';
 import { SmartContractModule } from './modules/smart-contract';
 import { UserModule } from './modules/user';
+import { MongoDbProviderModule } from './providers';
+import { PostgresProviderModule } from './providers/postgres.module';
 
 @Module({
   imports: [
@@ -18,26 +16,8 @@ import { UserModule } from './modules/user';
       rootPath: PUBLIC_FOLDER,
       serveRoot: '/static',
     }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [typeorm],
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        const config = configService.get<TypeOrmModuleOptions>('typeorm')!;
-        return { ...config, autoLoadEntities: true };
-      },
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        uri: config.get<string>('MONGODB_URI'),
-        dbName: config.get<string>('MONGODB_DATABASE'),
-      }),
-      inject: [ConfigService],
-    }),
-
+    MongoDbProviderModule,
+    PostgresProviderModule,
     JwtModule.register({
       global: true,
       secret: process.env.SECRET_KEY,
---
+++ api/src/providers/postgres.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+import { typeorm } from '@app/database/typeorm.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const config = configService.get<TypeOrmModuleOptions>('typeorm')!;
+        return { ...config, autoLoadEntities: true };
+      },
+    }),
+  ],
+})
+export class PostgresProviderModule {}

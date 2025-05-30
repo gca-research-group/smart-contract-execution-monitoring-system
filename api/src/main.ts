2e57771310279a3f36af156dfa09b52f18f589a6
Mailson Teles Borges <mailson.borges@sou.unijui.edu.br>
2025-05-30 09:32:15 -0300

    Change nest log to winston

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,38 @@
 import * as cookieParser from 'cookie-parser';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
 
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('Api', {
+              colors: true,
+              prettyPrint: true,
+              processId: true,
+              appName: true,
+            }),
+          ),
+        }),
+        new winston.transports.File({
+          level: 'verbose',
+          filename: 'app.log',
+          dirname: 'logs',
+        }),
+      ],
+    }),
+  });
 
   app.enableCors({
     credentials: true,
